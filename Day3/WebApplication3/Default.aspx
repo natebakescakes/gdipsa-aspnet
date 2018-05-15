591c9dccf80d401ac8e03737769fad85e4abca7e
natebakescakes <natebakescakes@gmail.com>
2018-05-15 10:06:46 +0800

    Add Day 3 Lecture

---
+++ Day3/WebApplication3/Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication3.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <%= DateTime.Now %><br />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </div>
+        <asp:Button ID="Button1" runat="server" Text="Button" />
+    </form>
+</body>
+</html>
---
+++ Day3/WebApplication3/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        int count;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ViewState["ct"] = 0;
+            }
+
+            int count = (int)ViewState["ct"];
+            count++;
+            ViewState["ct"] = count;
+            Label1.Text = count.ToString();
+        }
+    }
+}
